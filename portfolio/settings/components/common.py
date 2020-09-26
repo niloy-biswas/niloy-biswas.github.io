cec9baf31f4d71fc0802708147b3bf1ef03c993c
NILOY <niloy.swe@gmail.com>
2020-09-26 17:04:06 +0600

    commit updated

--- portfolio/settings/components/common.py
+++ portfolio/settings/components/common.py
@@ -111,7 +111,7 @@ LANGUAGES = (
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
