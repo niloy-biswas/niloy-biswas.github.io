6607f71e82697c6f6a9a45d738081db51dc3caf0
NILOY <niloy.swe@gmail.com>
2020-09-28 00:52:35 +0600

    host updated

--- portfolio/settings/components/common.py
+++ portfolio/settings/components/common.py
@@ -17,7 +17,7 @@ SECRET_KEY = '8=76-t2!^pcqyfs1a(n*m0f$jhgy_#i#h=$mwox7g$$&vu^f3g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '192.168.0.8', 'niloyportfolio.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '192.168.0.8', 'niloyportfolio.herokuapp.com', 'https://git.heroku.com/niloyportfolio.git']
 
 
 # Application definition
