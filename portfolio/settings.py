497e72878913c6852ce0ce7d79f11ed41b09d377
NILOY <niloy.swe@gmail.com>
2020-11-26 22:41:15 +0600

    update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -18,6 +18,8 @@ import django_heroku
 
 from decouple import config
 
+from importlib import reload
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
