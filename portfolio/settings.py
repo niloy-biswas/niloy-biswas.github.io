a4ce327428cdc3ee09dfa40d2dbd96c5a37127fc
NILOY <niloy.swe@gmail.com>
2020-11-30 02:00:34 +0600

    update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import django_heroku
 import os
 from pathlib import Path
 
-import django_heroku
+
 
 from decouple import config
 
