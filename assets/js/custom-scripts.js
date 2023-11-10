f715d05ec3a8004752319c1f9831fc48c3d147a2
Niloy Biswas <niloybiswas1999@gmail.com>
2023-11-10 14:49:32 +0600

    Fixed the filtering option In JS code and changes the section name in HTML

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -7,30 +7,30 @@
   
       /* Loader Code Start */
       $(window).on("load", function() { 
-          $(".section-loader").fadeOut("slow");
-          
-          var $container = $('.portfolioContainer');
-          $container.isotope({
-              filter: '*',
-              animationOptions: {
-                  queue: true
-              }
-          });
-       
-          $('.portfolio-nav li').click(function(){
-              $('.portfolio-nav .current').removeClass('current');
-              $(this).addClass('current');
-       
-              var selector = $(this).attr('data-filter');
-              $container.isotope({
-                  filter: selector,
-                  animationOptions: {
-                      queue: true
-                  }
-               });
-               return false;
-          });
+        $(".section-loader").fadeOut("slow");
+        
+        var $container = $('#mh-portfolio .portfolioContainer');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                queue: true
+            }
         });
+     
+        $('#mh-portfolio .portfolio-nav li').click(function(){
+            $('#mh-portfolio .portfolio-nav .current').removeClass('current');
+            $(this).addClass('current');
+     
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    queue: true
+                }
+             });
+             return false;
+        });
+      });
       /* Loader Code End */
 
 
