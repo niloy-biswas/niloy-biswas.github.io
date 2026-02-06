97435e89d2eab5e466989c3a15a6275bb550a794
Niloy <niloybiswas1999@gmail.com>
2026-02-06 18:30:55 +0600

    Removed unused files and code blocks

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -41,31 +41,7 @@
       // }
   
 
-      $(window).on('load', function() {
-        $('#header-slider #animation-slide').owlCarousel({
-               autoHeight: true,
-               items: 1,
-               loop: true,
-               autoplay: true,
-               dots: false,
-               nav: false,
-               autoplayTimeout: 3000,
-               navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-               animateIn: "zoomIn",
-               animateOut: "fadeOutDown",
-               autoplayHoverPause: false,
-               touchDrag: true,
-               mouseDrag: true
-           });
-         $("#animation-slide").on("translate.owl.carousel", function () {
-             $(this).find(".owl-item .slide-text > *").removeClass("fadeInUp animated").css("opacity","0");
-             $(this).find(".owl-item .slide-img").removeClass("fadeInRight animated").css("opacity","0");
-         });          
-         $("#animation-slide").on("translated.owl.carousel", function () {
-             $(this).find(".owl-item.active .slide-text > *").addClass("fadeInUp animated").css("opacity","1");
-             $(this).find(".owl-item.active .slide-img").addClass("fadeInRight animated").css("opacity","1");
-         });
-     });
+
    
     /*
     |====================
@@ -155,13 +131,6 @@
         .empty()
         .append('<i class="fa fa-circle"></i>');                
     });
-    
-    /*
-    |=================
-    | Portfolio mixin
-    |================
-    */   
-    $('#portfolio-item').mixItUp();
     
     /*
     |=================
